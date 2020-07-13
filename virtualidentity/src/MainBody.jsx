5e68739c33aa5f51be1b6bd502f20b8d24c032c9
joseedo <joseedo@gmail.com>
2020-07-13 21:48:41 +0100

    UI form

--- virtualidentity/src/MainBody.jsx
+++ virtualidentity/src/MainBody.jsx
@@ -16,10 +16,9 @@ class MainBody extends Component {
     key: '',
     email: '',
     phone: '',
-    address: ''
+    address: '',
+    isLoggedIn:false
   };
-
-
 
 captureName =(event) => {
   event.stopPropagation()
@@ -54,7 +53,6 @@ captureAddress =(event) => {
 convertToBuffer = async() => {
   //data is converted to a buffer for upload to IPFS
   const buffer = await Buffer.from(this.state.name + '¦' + this.state.key + '¦' + this.state.email + '¦' + this.state.phone + '¦' + this.state.address);
-  //set this buffer -using es6 syntax
   this.setState({buffer});
 };
 
@@ -73,76 +71,90 @@ onSubmit = async (event) => {
   event.preventDefault();
 }; //onSubmit 
 
-  render() {
+
+
+
+
+render() {
+
+ const createidentity = (
+    <div class="ui celled grid">
+    <div class="row">
+        <div class="three wide column">
+        Menu
+        </div>      
+         <div class="thirteen wide column">
+    <Header as="h2" textAlign="center">
+      Create New identity
+    </Header>
+    <Form size="large" onSubmit={this.onSubmit}>
+    <Segment padded>
+        <h3>Show identity</h3>
+        <Form.Input fluid icon="user" iconPosition="left" placeholder="name" onChange = {this.captureName} />
+    </Segment>
+    <Segment>
+        <h3>Key:</h3>
+        <Form.Input fluid icon="user" iconPosition="left" placeholder={this.state.key} onChange = {this.captureKey} />
+    </Segment>
+    <Segment>
+        <h3>Email:</h3>
+        <Form.Input fluid icon="e-mail" iconPosition="left" placeholder={this.state.email} onChange = {this.captureEmail} />
+    </Segment>
+    <Segment>
+        <h3>Phone:</h3>
+        <Form.Input fluid icon="phone" iconPosition="left" placeholder={this.state.phone} onChange = {this.capturePhone} />
+    </Segment>
+    <Segment>
+        <h3>Address:</h3>
+        <Form.Input fluid icon="address" iconPosition="left" placeholder={this.state.address} onChange = {this.captureAddress} />
+    </Segment>
+    <Segment>
+      <Button type="submit" bsStyle="primary" color="blue" fluid size="large">
+        Create Indentity
+      </Button>
+    </Segment>
+    </Form>
+    <hr/>
+    <Button onClick = {this.onClick}> Get Transaction Receipt </Button>
+    <Table bordered responsive>
+            <thead>
+              <tr>
+                <th>Tx Receipt Category</th>
+                <th>Values</th>
+              </tr>
+            </thead>
+           
+            <tbody>
+              <tr>
+                <td>IPFS Hash # stored on Eth Contract</td>
+                <td>TBD</td>
+              </tr>
+              <tr>
+                <td>Ethereum Contract Address</td>
+                <td>{this.state.ethAddress}</td>
+              </tr>
+              <tr>
+                <td>Tx Hash # </td>
+                <td>{this.state.transactionHash}</td>
+              </tr>
+              <tr>
+                <td>Block Number # </td>
+                <td>{this.state.blockNumber}</td>
+              </tr>
+              <tr>
+                <td>Gas Used</td>
+                <td>{this.state.gasUsed}</td>
+              </tr>
+            
+            </tbody>
+        </Table>
+    </div>
+    </div> 
+    </div>
+ );  
+
     return (
-      <Grid centered columns={2}>
-      <Grid.Column>
-        <Header as="h2" textAlign="center">
-          Create New identity
-        </Header>
-        <Form size="large" onSubmit={this.onSubmit}>
-        <Segment padded>
-            <h3>Show identity</h3>
-            <Form.Input fluid icon="user" iconPosition="left" placeholder="name" onChange = {this.captureName} />
-        </Segment>
-        <Segment>
-            <h3>Key:</h3>
-            <Form.Input fluid icon="user" iconPosition="left" placeholder={this.state.key} onChange = {this.captureKey} />
-        </Segment>
-        <Segment>
-            <h3>Email:</h3>
-            <Form.Input fluid icon="e-mail" iconPosition="left" placeholder={this.state.email} onChange = {this.captureEmail} />
-        </Segment>
-        <Segment>
-            <h3>Phone:</h3>
-            <Form.Input fluid icon="phone" iconPosition="left" placeholder={this.state.phone} onChange = {this.capturePhone} />
-        </Segment>
-        <Segment>
-            <h3>Address:</h3>
-            <Form.Input fluid icon="address" iconPosition="left" placeholder={this.state.address} onChange = {this.captureAddress} />
-        </Segment>
-        <Segment>
-          <Button type="submit" bsStyle="primary" color="blue" fluid size="large">
-            Create Indentity
-          </Button>
-        </Segment>
-        </Form>
-        <hr/>
-        <Button onClick = {this.onClick}> Get Transaction Receipt </Button>
-        <Table bordered responsive>
-                <thead>
-                  <tr>
-                    <th>Tx Receipt Category</th>
-                    <th>Values</th>
-                  </tr>
-                </thead>
-               
-                <tbody>
-                  <tr>
-                    <td>IPFS Hash # stored on Eth Contract</td>
-                    <td>TBD</td>
-                  </tr>
-                  <tr>
-                    <td>Ethereum Contract Address</td>
-                    <td>{this.state.ethAddress}</td>
-                  </tr>
-                  <tr>
-                    <td>Tx Hash # </td>
-                    <td>{this.state.transactionHash}</td>
-                  </tr>
-                  <tr>
-                    <td>Block Number # </td>
-                    <td>{this.state.blockNumber}</td>
-                  </tr>
-                  <tr>
-                    <td>Gas Used</td>
-                    <td>{this.state.gasUsed}</td>
-                  </tr>
-                
-                </tbody>
-            </Table>
-        </Grid.Column>
-        </Grid>
+        createidentity
       );
   }
 } 
