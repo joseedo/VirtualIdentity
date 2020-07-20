3581282b3bcef2206e2de0b3eb5477d845f1520d
joseedo <joseedo@gmail.com>
2020-07-20 22:05:07 +0100

    added web3 and ipfs connection

---
+++ virtualidentity/src/storehash.js
@@ -0,0 +1,86 @@
+import web3 from './web3';
+//access our local copy to contract deployed on rinkeby testnet
+//use your own contract address
+const address = '0xa36d612ca9e3f176b7e7fdc293f0433b2ba87155';
+//use the ABI from your contract
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "Ipfshash",
+				"type": "bytes32"
+			}
+		],
+		"name": "CreateIdentity",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "GetIdentity",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "x",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "IdArray",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
